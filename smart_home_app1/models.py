13ee390032d2a9afc1617a4c412ee3f2bf2f13e1
Виталий Чухляев <vitaliy69@mail.ru>
2023-03-05 16:51:43 +0300

    Created models: 'Facility', 'Measurement'

--- smart_home_app1/models.py
+++ smart_home_app1/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Facility(models.Model):
+
+    name = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+
+class Measurement(models.Model):
+
+    value = models.FloatField()
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
