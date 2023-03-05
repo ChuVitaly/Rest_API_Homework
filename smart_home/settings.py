32fbb2aba63a7282058595907d33d8c7f76e06ab
Виталий Чухляев <vitaliy69@mail.ru>
2023-03-05 16:11:08 +0300

    INSTALLED_APPS

--- smart_home/settings.py
+++ smart_home/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'smart_home_app1',
 ]
 
 MIDDLEWARE = [
