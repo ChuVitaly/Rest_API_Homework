02050d74bf1d93eb55724edd5a12d8c268eaa993
Виталий Чухляев <vitaliy69@mail.ru>
2023-03-13 14:52:04 +0300

    redid views.py(generics.ListAPIView).

--- smart_home_app1/views.py
+++ smart_home_app1/views.py
@@ -1,15 +1,20 @@
-from rest_framework import viewsets
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets, generics
+
 
 from smart_home_app1.models import Sensor, Measurement
 from smart_home_app1.serializers import SensorSerializer, MeasurementSerializer
 
 
-class SensorViewSet(viewsets.ModelViewSet):
+class SensorList(generics.ListAPIView):
     queryset = Sensor.objects.all()
     serializer_class = SensorSerializer
 
 
-class MeasurementViewSet(ModelViewSet):
+class MeasurementList(generics.ListAPIView):
     queryset = Measurement.objects.all()
     serializer_class = MeasurementSerializer
+
+
+class SensorDetail(generics.RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
